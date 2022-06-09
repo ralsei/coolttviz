a47e038f649bbafe13e8edf56c7318fdbce683a7
hazel levine <hazel@knightsofthelambdacalcul.us>
2022-06-09 14:49:12 -0500

    add initial flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, fenix, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        defaultPackage = (pkgs.makeRustPlatform {
+          inherit (fenix.packages.${system}.minimal) cargo rustc;
+        }).buildRustPackage {
+          pname = "coolttviz";
+          version = "0.1.0";
+          src = ./.;
+          cargoSha256 = nixpkgs.lib.fakeSha256;
+        };
+      });
+}
